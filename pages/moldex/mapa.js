d69caa369b6ef5fab11e8e1280a406e69c92249e
EdwinFloresVega <92103867+EdwinFloresVega@users.noreply.github.com>
2022-02-14 16:00:47 -0600

    fuga

--- pages/moldex/mapa.js
+++ pages/moldex/mapa.js
@@ -3,6 +3,7 @@ import {useState, useEffect} from 'react'
 import ReactDOM from 'react-dom';
 import React from 'react';
 import Image from 'next/image'
+import {Auth, withSSRContext,API} from 'aws-amplify';
 import  {element, Component} from 'react' 
 import Animate from "react-move/Animate";
 
@@ -15,47 +16,112 @@ if (typeof window !== 'undefined') {
     console.log('You are on the server')
   }
 
-function Mapa(){
+function Mapa({token}){
+  //console.log(token)
   const [name, setName] =useState([])
-  let [x,SetX]=useState(22)
-  let [y,SetY]=useState(150)
-  const px=[22,30,35,40,45,50,55,100]
-  const py=[150,160,160,160]
+  let [x,SetX]=useState(0)//x- origen -224
+  let [y,SetY]=useState(0)//y- origen -496
+  let[bateria,setBateria]=useState()
+  let rplazo
+  let separarD
   let i=0
 
 
+//   function AGVDatos(e){
+//     console.log(e)
+//     let rplazo= e.replace(/["{}]/g,"")
+//     let separarD=rplazo.split(',')
+//     console.log("Mis datos: "+separarD)
+//     //let Bateria2=separarD[12].split(':');
+//     let x=separarD[1].split(':');
+//     let y=separarD[2].split(':');
+//     console.log('Mi X: ',x[1],' Mi Y: ',y[1])
+//     console.log((x[1]/1000))
+//     console.log((x[1]/1000)-224)
+//     SetX((x[1]/1000)-224)
+//     console.log((y[1]/1000))
+//     console.log((y[1]/1000)-496)
+//     SetX((y[1]/1000)-496)
+//     //console.log('Mi bateria es: '+ Bateria2[1])
+// }
   setInterval(() => {
-    console.log(px[1])
-    SetX(px[1])
-    console.log(py[1])
-    SetY(py[1])
-    console.log(px[1])
-    SetX(px[2])
-    console.log(py[1])
-    SetY(py[2])
-    SetX(px[3])
-    SetX(px[4])
-    SetX(px[5])
-    SetX(px[6])
-    SetX(px[7])
 
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Authorization': token },
+      body: JSON.stringify({ "thing":"M5GPS" })
+  };  
+  fetch('https://intxgh6og0.execute-api.us-east-1.amazonaws.com/servs/leer', requestOptions)
+  .then(res => res.json()) 
+    .then(res => {
+      
+        console.log(res.body);
+        if(res.body==undefined){
+          console.log('sin datos brooooo')
+        }else{
+        rplazo= res.body.replace(/["{}]/g,"")
+     separarD=rplazo.split(',')
+    console.log("Mis datos: "+separarD)
+    let x=separarD[1].split(':');
+    let y=separarD[2].split(':');
+    let bat=separarD[12].split(':');
+    console.log('Mi X: ',x[1],' Mi Y: ',y[1])
+    console.log('bateria:',bat[1])
+    setBateria(bat[1])
+    //crear set para bateria
+    console.log('mi xssa:',(((x[1])/100)-224)*(-1))
+  
+    SetX((((x[1])/100)-224)*(-1) )
 
+    console.log('mi yssa:',(((y[1])/100)-496)*(-1))
+    SetY((((y[1])/100)-496)*(-1))
 
-   
-   }, 5000);
+  }
 
-  useEffect(()=>{
-    // const configPOST = {method: 'POST', headers:{"Authorization" : 'eyJraWQiOiJBcHgzMW5YRmlcL0o0d1wvZER2d2FMSDVuYytMeVdwaEF4MlYxWlhyRGx1M0k9IiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiJjYTY0MTkwMC1lN2QxLTRjMGYtYmVjYS1kOTBjN2VkZGYwY2UiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLnVzLWVhc3QtMS5hbWF6b25hd3MuY29tXC91cy1lYXN0LTFfR3lhVGlhTUM2IiwiY29nbml0bzp1c2VybmFtZSI6ImNhbGViIiwib3JpZ2luX2p0aSI6IjhjNjU4ZjBmLWM4NGItNGZhNy05NTQ0LTNmOTdlMzc2M2E0MiIsImF1ZCI6IjFxMnJvY2U2YTE4a3Q5aW9qZzlzN3FvOHBlIiwiZXZlbnRfaWQiOiJlYzgzODFhNi02NjVlLTQ2ZjktOWE3YS04MzMyMDZjNTQzNjgiLCJ0b2tlbl91c2UiOiJpZCIsImF1dGhfdGltZSI6MTY0MjAwNDc4NSwiZXhwIjoxNjQ0NTE1MDgxLCJpYXQiOjE2NDQ1MTE0ODEsImp0aSI6ImU0MTM5ZDcyLWM3ZDYtNGYzYS04MWM2LWZiZDk3NGE5ODBiNyIsImVtYWlsIjoiYzQxZTNvQGdtYWlsLmNvbSJ9.AJOQjmxYV6f2GZq8bB6nJTnAcKO6f90BAALZ6ql8dBCUQ1EpX5ezrtPhO7HsQc9Z04-tPCks9wLUaze0SOsfw9RNwhnp0RuTdH--eTNWs6lxiHRCN8A4OmhrXm4cUK1WFzEBZmd2yAjNoIcvgoAVl08mQHjrDvMKags1E1g5_3ImrG00PimCM0A2iVdbWNyivxai8qgJGPV9t00sxncXwj3LDQxxdoKIkfdXa-CRoWRWBEZoAWL5Z6NSn021LSBu_-rU-f8xXj4PPTmljFP71nulg1NgB5--DpvR3r-GW5o1W0xabVqO138Y66fsVzaRrC014-1kFybknTMEZrg8VA' }, body:JSON.stringify({thing:"M5GPS", datos:["x","y"]})};
+     })
+     
+     .catch(() => {
+      //alert('failed to fetch');
+      console.log('ERROR DE CONEXION *********************')
+      i=i+1
+      console.log(i)
+      if(i==40){
+        window.location.reload();
+      }
+    });
+     
+     
+     ;
+  
+  
+    // const configPOST = {method: 'POST', headers:{"Authorization" : token }, body:JSON.stringify({thing:"M5GPS"})};
 
-    // fetch("https://k9gnk9tdra.execute-api.us-east-1.amazonaws.com/servicios/leer-sombras",configPOST)
+    // fetch("https://intxgh6og0.execute-api.us-east-1.amazonaws.com/servs/estadoagv",configPOST)
 
-    //   .then(res => res.text())
-
+    //   .then(res => res.json())
     //   .then(res => {
 
-    //     console.log(res);
+    //     console.log(res.body);
+    //     let rplazo= res.body.replace(/["{}]/g,"")
+    // let separarD=rplazo.split(',')
+    // console.log("Mis datos: "+separarD)
+    // let x=separarD[1].split(':');
+    // let y=separarD[2].split(':');
+    // console.log('Mi X: ',x[1],' Mi Y: ',y[1])
+    // console.log('mi xssa:',((y[1])/1000)-496)
 
-    //   });
+    //  });
+
+   
+   }, 1300);
+
+   function imprime(){
+     console.log('lalalalalalallalalalalal')
+     
+   }
+
+  useEffect(()=>{
+    
   
 }, []);
 
@@ -69,12 +135,13 @@ function Mapa(){
             enter={{ cx:x, cy: y }}
             update={{ cx: x, cy: y }}
           >
-            {d => <circle cx={d.cx} cy={d.cy} r="10" stroke="black"  fill="red" />}
+            {d => <circle cx={d.cx} cy={d.cy} r="10" stroke="black" fill="red" onclick={imprime()}> <title>AGV1: {bateria}</title></circle> }
           </Animate>
         </svg>
         <Image src={require('/image/mprueba.png')}   width="231" height="625" objectFit='cover' />
         
      </div>
+
 
         
     </Layout>
@@ -85,4 +152,22 @@ function Mapa(){
   
   
   export default Mapa
-  
+
+  export async function getServerSideProps({req}) {
+    const {Auth,API} = withSSRContext({req});
+    try{
+        const user = await Auth.currentAuthenticatedUser();
+        
+        return{
+            props:{
+                authenticated: true,
+                user: user.username,
+                token: user.signInUserSession.idToken.jwtToken
+            }
+        }
+    }catch(err){
+        return{
+            props:{authenticated: false}
+        }
+    }
+  }
