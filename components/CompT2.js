fdbe38aeff5a4f54e9b21e277d5bc0187f1accc5
CalebOr <caleb.ortiz.mondragon@gmail.com>
2022-06-24 13:12:30 -0500

    first working version

--- components/CompT2.js
+++ components/CompT2.js
@@ -5,123 +5,8 @@ import Image from 'next/image';
 import Swal from 'sweetalert2';
 import ReactDOM from "react-dom";
 
-export default function ContentFormTar({id, tarima}) {
-    const [val1, setVal1]=useState(0)
-    const [val2, setVal2]=useState(0)
-    const [val3, setVal3]=useState(0)
-    const [val4, setVal4]=useState(0)
-    const [val5, setVal5]=useState(0)
-    const [val6, setVal6]=useState(0)
-    const [val7, setVal7]=useState(0)
-    const [val8, setVal8]=useState(0)
-    const [val9, setVal9]=useState(0)
-    const [val10, setVal10]=useState(0)
-    const [val11, setVal11]=useState(0)
-    const [val12, setVal12]=useState(0)
-    const [hora, setHora]=useState()
-    const [dia2, setDia2]=useState('2022-03-04')
+export default function ContentFormTar({id, childToParent}) {
     const [inputList, setInputList] = useState([]);
-
-
-    function setValues(a){
-        console.log("Hola: "+a)
-        setVal1(a)
-    }
-    async function LiberarE(){
-        setVal2(0)
-        setVal3(0)
-        setVal4(0)
-        setVal5(0)
-        setVal6(0)
-        setVal7(0)
-        setVal8(0)
-        setVal9(0)
-        setVal10(0)
-        setVal11(0)
-        setVal12(0)
-    }
-    async function EnvioDatos(){
-        if (dia2==undefined || hora==undefined || val1==undefined){
-            Swal.fire({
-                icon: 'error',
-                title: 'Falta fecha, hora o seleccionar el espacio',
-                showConfirmButton: false,
-                timer: 1500
-              })
-        }else{
-            if (val1=='1'){
-                setVal2(1)
-            }else if (val1=='5'){
-                setVal3(1)
-            }else if (val1=='8'){
-                setVal4(1)
-            }else if (val1=='8'){
-                setVal5(1)
-            }else if (val1=='9'){
-                setVal6(1)
-            }else if (val1=='10'){
-                setVal7(1)
-            }else if (val1=='11'){
-                setVal8(1)
-            }else if (val1=='12'){
-                setVal9(1)
-            }else if (val1=='13'){
-                setVal10(1)
-            }else if (val1=='14'){
-                setVal11(1)
-            }else if (val1=='15'){
-                setVal12(1)
-            }
-            let hora2= hora;
-            let day= dia2.replace(/-/g,".");
-            let format=day+' '+hora2;
-            let unix= parseInt((new Date(format).getTime() / 1000).toFixed(0))
-            let ruta=id+"_"+val1
-
-            console.log('Mi fecha: ', format,' a Formato unix: '+unix)
-            const requestOptions = {
-                    method: 'POST',
-                    body: JSON.stringify({
-                        "tabla": "misiones",
-                        "datos": {
-                        "objetivo":id ,
-                        "prioridad": 0,
-                        "idagv": 2,
-                        "timestamp": unix}
-                        },
-                        )
-                };  
-            const requestOptions2 = {
-                method: 'POST',
-                body: JSON.stringify({
-                    "tabla": "misiones",
-                    "datos": {
-                    "objetivo":val1 ,
-                    "prioridad": 0,
-                    "idagv": 2,
-                    "timestamp": unix}
-                    },
-                    )
-            }; 
-            console.log(requestOptions)
-                fetch('http://localhost:5000/enviarruta', requestOptions)
-                .then(res => res.json()) 
-                .then(function(res2){
-                        fetch('http://localhost:5000/enviarruta', requestOptions2)
-                        .then(res2 => res2.json()) 
-                        .then(res2 => console.log(res2));
-                });
-                // fetch('https://intxgh6og0.execute-api.us-east-1.amazonaws.com/servs/enviarruta', requestOptions2)
-                // .then(res => res.json()) 
-                // .then(res => console.log(res));
-                Swal.fire({
-                    icon: 'success',
-                    title: 'Envio de Ruta',
-                    showConfirmButton: false,
-                    timer: 1500
-                  })
-        }
-    }
 
     useEffect(()=>{
         axios.post('http://localhost:5000/asknodes')
@@ -129,11 +14,11 @@ export default function ContentFormTar({id, tarima}) {
             var listTemp = [];
             for(const x in res.data){
                 const description = res.data[x].Description;
-                const id = res.data[x].Id;
                 if(res.data[x].Description.length>12){
                     continue;
                 }
-                listTemp.push(<button id={'Esp'+id} className='border-4 border-solid border-indigo-50' onClick={(e)=>setValues(id)} style={{backgroundColor:(val1=='1'?'#4F91BB':val2=='1'?'#923128':'')}}><span className='text-white text-sm sm:text-3xl md:text-4xl lg:text-4xl xl:text-5xl 2xl:text-6xl'>{description}</span></button>);
+                res.data[x].caller = id;
+                listTemp.push(<button className='border-4 border-solid border-indigo-50' onClick={(e)=>childToParent(res.data[x])}><span className='text-white text-sm sm:text-3xl md:text-4xl lg:text-4xl xl:text-5xl 2xl:text-6xl'>{description}</span></button>);
             }
             setInputList(listTemp);
         });
@@ -146,34 +31,17 @@ export default function ContentFormTar({id, tarima}) {
                     <div className=' items-center w-[105%] p-1 border-indigo-400 rounded-md border-[2px]'>
                         <div className="grid grid-cols-1 lg:grid-cols-1 md:grid-cols-1 w-[100%]">
                             <div className="grid justify-items-center m-1">
-                                <div className=" justify-between text-justify grid-cols-2 flex ">
-                                    <label className="font-light text-center">Fecha de inicio: &nbsp;&nbsp; </label><br/>
-                                    <input type="date"  onChange={(e)=>setDia2(e.target.value)} className="w-[200px] h-[25px] focus:outline-none focus:shadow-outline border-[1px] border-[#c5c6d8] dark:text-black dark:bg-[whitesmoke] rounded "/>&nbsp;
-                                    <input type="time"  onChange={(e)=>setHora(e.target.value)} className="w-[200px] h-[25px] focus:outline-none focus:shadow-outline border-[1px] border-[#c5c6d8] dark:text-black dark:bg-[whitesmoke] rounded "/>
-                                    <br/>
-                                </div><br/>
                                 <div className="i relative text-center">
                                     <h1 className="text-center">Seleccione el destino</h1>
-                                    {tarima==1&&(
-                                        <div className='grid grid-cols-3 top-[10%] left-2 text-center h-[40%] absolute w-[70%] z-10 '>
-                                            {inputList}
-                                        </div>
-                                    )}
-                                    
-                                    
+                                    <div className='grid grid-cols-3 top-[10%] left-2 text-center h-[40%] absolute w-[70%] z-10 '>
+                                        {inputList}
+                                    </div>
                                     <Image  src={require('../image/posicion.png')}></Image><br/>
                                 </div>
-
-
-
                             </div>
                         </div>
                         <br/>
-                        <div className='flex flex-col items-center p-1'>
-                                <button  type='button' onClick={(e)=>EnvioDatos()} className='h-8 border-[1px] border-yellow-400 bg-yellow-500 hover:bg-yellow-600 text-white rounded-md px-2'>Continuar</button>
-                            </div>
                     </div>
-
                 </div>
             </div>
         </div>
--- pages/moldex/tarimas2.js
+++ pages/moldex/tarimas2.js
@@ -5,44 +5,92 @@ import CompTarima from "../../components/CompTarima";
 import ContentFormTar from "../../components/CompT2";
 import Image from "next/image";
 import Select from "react-select";
+import axios from "axios";
 import { useEffect, useState } from "react";
+import Swal from 'sweetalert2';
+
+
 function Tarimas(){
     const [modal, setModal]= useState(false);
     const [modal2, setModal2]= useState(false);
-    const [modalT, setModalT]=useState(false);
-    const [inicio, setInicio] =useState('')
+    const [modal3, setModal3]= useState(false);
+    const [start, setStart]= useState();
+    const [end, setEnd]= useState();
     const [tarima, setTarima]=useState(1)
     const [title, setTitle] =  useState('')
     const [Bateria, setBateria] = useState('')
     const [val1, setVal1]=useState(1)
     const [val2, setVal2]=useState(1)
-    const [val3, setVal3]=useState(1)
     const [Home, setHome] = useState()
+    const [data, setData] = useState('')
+    const [selectingStart, setSelectingStart] = useState(true)
 
     const [Espacio, setEspacio] =useState(10)
     
     async function changeModal(){
         setModal(!modal)
     }
-    async function changeModal2(e,id){
+    async function changeModal2(e){
+        console.log("e: "+e)
         setTitle(e)
-        setInicio(id)
         setModal2(!modal2)
-        if(id==2){
+        if(e == 'Inicio'){
             setVal1(0)
-        }else if (id==0){
+        }else if(e == 'Fin'){
             setVal2(0)
-        }else if (id==13){
-            setVal3(0)
         }
     }
-    async function Liberar(){
-        setVal1(1)
-        setVal2(1)
-        setVal1(1)
+    async function changeModal3(e){
+        console.log("e: "+e)
+        setTitle(e)
+        setModal3(!modal3)
+        if(e == 'Inicio'){
+            setVal1(0)
+            setSelectingStart(true)
+        }else if(e == 'Fin'){
+            setVal2(0)
+            setSelectingStart(false)
+        }
     }
+
+    async function sendMission(){
+        axios.post('http://localhost:5000/nuevaruta',{
+            "start": start,
+            "end": end
+        })
+        .then(res => {
+            console.log(res.data)
+            Swal.fire({
+                icon: 'success',
+                title: 'Ruta enviada',
+                showConfirmButton: false,
+                timer: 1500
+            })
+        })
+        .catch(err => {
+            console.log(err)
+            Swal.fire({
+                icon: 'error',
+                title: 'Error al crear ruta',
+                showConfirmButton: false,
+                timer: 1500
+              })
+        });
+    }
+
+    const childToParent = (childdata) => {
+        console.log("Child says: ",childdata);
+        if(childdata.caller == 'start'){
+            setStart(childdata.Id)
+        }else if(childdata.caller == 'end'){
+            setEnd(childdata.Id)
+        }
+        setModal2(false);
+        setModal3(false);
+    }
+
     function AGVDatos(e){
-        console.log(e)
+        console.log("AGVDatos(e):",e)
         const dataJSON = e.data[0];
         let Bateria2 = dataJSON['bat'];
         let IdAGV = dataJSON['bat'];
@@ -62,24 +110,6 @@ function Tarimas(){
             setBateria('Recargando AGV ## %')
         }
     }
-    async function RegresarHome(){
-        const requestOptions = {
-            method: 'POST',
-            body: JSON.stringify({
-                "tabla": "misiones",
-                "datos": {
-                "objetivo":1 ,
-                "prioridad": 0,
-                "idagv": 2,
-                "timestamp": 5}
-                },
-                )
-        };  
-        console.log('Sending new route',requestOptions)
-        fetch('http://localhost:5000/nuevaruta', requestOptions)
-        .then(res => res.json())
-        .then(res => console.log(res));
-    }
    
     useEffect(() => {
         const requestOptions = {
@@ -89,7 +119,13 @@ function Tarimas(){
         fetch('http://localhost:5000/estadoagv', requestOptions)
         .then(res => res.json())
         .then(res =>AGVDatos(res));
-
+        const requestOptions2 = {
+            method: 'POST',
+            body: JSON.stringify({ "thing":"M5GPS" })
+        };
+        fetch('http://localhost:5000/actualizarNodos', requestOptions2)
+        .then(res => res.json())
+        .then(res =>console.log(res));
     }, [])
     return(
         <Layout>
@@ -100,7 +136,10 @@ function Tarimas(){
                 <br/>
             </Modal>
             <Modal active={modal2} title={title} action={changeModal2}>
-                <ContentFormTar id={inicio} tarima={tarima}/>
+                <ContentFormTar id={"start"} childToParent={childToParent}/>
+            </Modal>
+            <Modal active={modal3} title={title} action={changeModal3}>
+                <ContentFormTar id={"end"} childToParent={childToParent}/>
             </Modal>
             <div className="grid-col-3 flex ">
                 <button
@@ -112,34 +151,30 @@ function Tarimas(){
                     <p className="text-xl text-gray-600">Nivel de batería: {Bateria}</p>
                     <br/>
                 </div>
-                <button
+                {/* <button
                     className="text-[white]  border-[#2ca577] bg-[#3b9174] h-[15%] rounded-md"
-                    onClick={()=>RegresarHome()} >Regresar a Home</button>
+                    onClick={()=>RegresarHome()} >Regresar a Home</button> */}
             </div>
             
-
             <div className=" w-[75vw] h-[80vh] ">
-                    <div className="grid grid-cols-3 gap-4 h-[22vh]">
-                        <div className="bg-[#4E5255] border-[white] border-solid border-2" style={{backgroundColor:(val1=='0'?'#923128':'')}}>
-                            <CompTarima id={'posE'} aux={1} letra={'Inicio'} action={()=>changeModal2('Ocupar espacio 1','2')} />
-                        </div>
-                        {/* <div className="bg-[#65686B] border-[white] border-solid border-2" style={{backgroundColor:(val2=='0'?'#923128':'')}}>
-                            <CompTarima id={'posF'} aux={1} letra={'2'} action={()=>changeModal2('Ocupar espacio 2','10')}/>
-                        </div>
-                        <div className="bg-[#4E5255] border-[white] border-solid border-2" style={{backgroundColor:(val3=='0'?'#923128':'')}}>
-                            <CompTarima id={'posG'} aux={1} letra={'3'} action={()=>changeModal2('Ocupar espacio 3','13')}/>
-                        </div> */}
+                <div className="grid grid-cols-3 gap-4 h-[22vh]">
+                    <div className="bg-[#4E5255] border-[white] border-solid border-2" style={{backgroundColor:(val1=='0'?'#4F91BB':'')}}>
+                        <CompTarima id={'posE'} aux={1} letra={'Inicio'} action={()=>changeModal2('Inicio')} />
                     </div>
-                    <br/>
-                    <br/>
-                    <div></div>
-                    <button
-                        className="text-[white]  border-[#2ca577] bg-[#3b9174] w-[10%] h-[10%] rounded-md"
-                        onClick={()=>Liberar()} >Liberar Espacios</button>
-                    
-                    <div className="text-[black] text-5xl text-right">
-                        <span>{Espacio} Espacios Disponibles</span>
+                    <div className="bg-[#65686B] border-[white] border-solid border-2" style={{backgroundColor:(val2=='0'?'#4F91BB':'')}}>
+                        <CompTarima id={'posF'} aux={1} letra={'Fin'} action={()=>changeModal3('Fin')}/>
                     </div>
+                </div>
+                <br/>
+                <br/>
+                <div></div>
+                <button
+                    className="text-[white]  border-[#2ca577] bg-[#3b9174] w-[10%] h-[10%] rounded-md"
+                    onClick={()=>sendMission()} >Enviar misión</button>
+                
+                <div className="text-[black] text-5xl text-right">
+                    <span>{Espacio} Espacios Disponibles</span>
+                </div>
             </div>
 
         </Layout>
